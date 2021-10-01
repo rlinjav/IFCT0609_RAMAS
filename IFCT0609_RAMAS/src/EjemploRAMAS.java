7e16825677d2163eb6e943ab567b8d465f8a50b7
tamsu <tamsu@LAPTOP-KH477OF4>
2021-10-01 20:43:20 +0200

    mensaje de prueba de Daniel

--- IFCT0609_RAMAS/src/EjemploRAMAS.java
+++ IFCT0609_RAMAS/src/EjemploRAMAS.java
@@ -5,7 +5,7 @@ public class EjemploRAMAS {
 		System.out.println("CAMBIO");
 		System.out.println("Prueba 1 de repositorio ");
 		System.out.println("Saludos de tiomao");
-		System.out.println("Daniel hace un cambio");
+		System.out.println("Daniel hace un otro cambio mas");
 	}
 
 }
