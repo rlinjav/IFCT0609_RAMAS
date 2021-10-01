b651db74334f5b87a73cc26e2513301718e15863
misa_ <misa_@192.168.1.173>
2021-10-01 19:59:23 +0200

    Paula Branch de prueba

--- IFCT0609_RAMAS/src/EjemploRAMAS.java
+++ IFCT0609_RAMAS/src/EjemploRAMAS.java
@@ -4,6 +4,8 @@ public class EjemploRAMAS {
 	public static void main(String[] args) {
 		System.out.println("CAMBIO");
 		System.out.println("Prueba 1 de repositorio ");
+		
+		System.out.println("Esto es un mensaje nuevo para hacer pruebas con muchas ramas");
 	}
 
 }
