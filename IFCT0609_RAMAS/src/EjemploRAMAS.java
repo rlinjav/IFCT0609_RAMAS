72b8c9e16217454264ffd6631f1e929cf16cd0b5
tiomao <tiomao@DESKTOP-C1MPS3K>
2021-10-01 20:13:28 +0200

    mao

--- IFCT0609_RAMAS/src/EjemploRAMAS.java
+++ IFCT0609_RAMAS/src/EjemploRAMAS.java
@@ -6,6 +6,7 @@ public class EjemploRAMAS {
 		System.out.println("Prueba 1 de repositorio ");
 		System.out.println("Saludos de tiomao");
 		System.out.println("Daniel hace un cambio");
+		System.out.println("Segundo saludo de tiomao");
 	}
 
 }
